6a208333e773dcfd5bda959ff57fde33fc927909
Trung Luu <luuductrung1234@gmail.com>
2022-08-05 22:38:57 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    motor.servo(motor.Servos.S1, 45)
+    motor.servo(motor.Servos.S1, 180)
+    basic.pause(200)
+    motor.servo(motor.Servos.S1, 0)
+    basic.showIcon(IconNames.Square)
+})
+input.onButtonPressed(Button.B, function () {
+    motor.servo(motor.Servos.S8, 180)
+    basic.pause(200)
+    motor.servo(motor.Servos.S1, 0)
+    basic.showIcon(IconNames.SmallSquare)
 })
 basic.forever(function () {
     basic.showIcon(IconNames.Happy)
