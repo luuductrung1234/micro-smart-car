0da4621cbd62db7ccc9a5a9c98f754aebe5c3b22
Trung Luu <luuductrung1234@gmail.com>
2022-08-06 09:55:38 +0700

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     motor.servo(motor.Servos.S1, 180)
     basic.pause(200)
     motor.servo(motor.Servos.S1, 0)
     basic.showIcon(IconNames.Square)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     motor.servo(motor.Servos.S8, 180)
     basic.pause(200)
     motor.servo(motor.Servos.S1, 0)
     basic.showIcon(IconNames.SmallSquare)
 })
-basic.forever(function () {
+basic.forever(function on_forever() {
     basic.showIcon(IconNames.Happy)
 })
