2ff304418c38657f4e40f51704d851b6a1011608
Trung Luu <luuductrung1234@gmail.com>
2022-08-06 14:50:17 +0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
+# ========================================
+# BASIC
+# ========================================
+
 def on_start():
-    basic.show_icon(IconNames.HEART)
+    basic.show_icon(IconNames.HAPPY)
+    radio.set_group(2208061444)
+
+def on_forever():
+    basic.show_string("C") # Car
+    
+basic.forever(on_forever)
+
+# ========================================
+# MAIN
+# ========================================
+
+def handle_street_sign(sign: number):
+    # Sign Code
+    # 0: stop
+    # 1: right
+    # 2: left
+    # 3: back
+    basic.show_icon(IconNames.ASLEEP)
+
+# ========================================
+# RADIO
+# ========================================
+
+def on_received_value(name, value):
+    pass
+
+radio.on_received_value(on_received_value)
+
+# ========================================
+# BUTTON
+# ========================================
 
 def on_button_pressed_a():
     motor.servo(motor.Servos.S1, 180)
     basic.pause(200)
     motor.servo(motor.Servos.S1, 0)
     basic.show_icon(IconNames.SQUARE)
-input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     motor.servo(motor.Servos.S8, 180)
     basic.pause(200)
     motor.servo(motor.Servos.S1, 0)
     basic.show_icon(IconNames.SMALL_SQUARE)
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def on_forever():
-    basic.show_icon(IconNames.HAPPY)
-    radio.set_group(1)
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,36 @@
+//  ========================================
+//  BASIC
+//  ========================================
 function on_start() {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Happy)
+    radio.setGroup(2208061444)
 }
 
+//  Car
+basic.forever(function on_forever() {
+    basic.showString("C")
+})
+//  ========================================
+//  MAIN
+//  ========================================
+function handle_street_sign(sign: number) {
+    //  Sign Code
+    //  0: stop
+    //  1: right
+    //  2: left
+    //  3: back
+    basic.showIcon(IconNames.Asleep)
+}
+
+//  ========================================
+//  RADIO
+//  ========================================
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    
+})
+//  ========================================
+//  BUTTON
+//  ========================================
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     motor.servo(motor.Servos.S1, 180)
     basic.pause(200)
@@ -13,8 +42,4 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     basic.pause(200)
     motor.servo(motor.Servos.S1, 0)
     basic.showIcon(IconNames.SmallSquare)
-})
-basic.forever(function on_forever() {
-    basic.showIcon(IconNames.Happy)
-    radio.setGroup(1)
 })
